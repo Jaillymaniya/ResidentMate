c415c7b3b0b7cac26bb9253db222c1887036ab8a
jaillymaniya <jelimaniya@gmail.com>
2025-12-20 11:43:17 -0500

    login error solution

--- client/src/homedashboard/Login.jsx
+++ client/src/homedashboard/Login.jsx
@@ -47,7 +47,7 @@ export default function Login() {
     setMessage("");
 
     try {
-      const res = await fetch(`${API_BASE}/api/auth/login`, {
+      const res = await fetch(`https://residentmate.onrender.com/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
