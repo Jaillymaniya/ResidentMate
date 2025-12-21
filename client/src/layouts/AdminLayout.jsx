746d55b05ccb298efdde54f9c055c5da0d29b837
jaillymaniya <jelimaniya@gmail.com>
2025-12-21 01:43:41 -0500

    admin sidebar space management

--- client/src/layouts/AdminLayout.jsx
+++ client/src/layouts/AdminLayout.jsx
@@ -150,7 +150,7 @@ export default function AdminLayout({ children }) {
         .sidebar .menu li {
           margin: 1rem 0;
           margin-left: 10px;
-          margin-bottom: 30px;
+          margin-bottom: 20px;
         }
         .sidebar .menu a {
           text-decoration: none;
